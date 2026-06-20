d56b3fe381bbfdfb443cd4bfe4f5594062f7906a
Krishna-Kr-Yadav <krish.yad2005@gmail.com>
2026-06-20 10:06:39 +0530

    Corrected the name of JWT secret key

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -19,7 +19,7 @@ async function authUser(req,res,next){
     }
 
     try{
-        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY)
 
         req.user = decoded
 
